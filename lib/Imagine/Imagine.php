f4f7675cf3dc5b1d622db09d825158f0e4272f8d
Alfonso de la Osa <arfonzi@gmail.com>
2011-02-08 14:38:15 +0100

    at least public

--- lib/Imagine/Imagine.php
+++ lib/Imagine/Imagine.php
@@ -2,6 +2,32 @@
 
 require_once("Core.php"); 
 
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * http://www.opensource.org/licenses/lgpl-license.php.
+ */
+
+/**
+ * The base setup class of Imagine
+ *
+ * @package     Imagine
+ * @author      Alfonso de la Osa <alfonso.delaosa@gmail.com>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        https://github.com/botverse/imagine
+ */
 class Imagine extends ImagineCore {
     
     public function configureTools() {
@@ -19,11 +45,10 @@ class Imagine extends ImagineCore {
     }
     public function configureFonts(){
         return array(
-            'arial' => 'arial',
-            'arial_bold' => 'arialbd',
-            'arial_black' => 'arialblk',
-            'arial_italic' => 'ariali',
-            'arial_bold_italic' => 'arialbi'
+            'sans' => 'sans',
+            'sans_bold' => 'sansbd',
+            'sans_italic' => 'sansi',
+            'sans_bold_italic' => 'sansbi'
         );
     }
     
